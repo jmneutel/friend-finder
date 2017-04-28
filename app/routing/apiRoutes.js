b073236424f97cdb1f952ab5ca41558ae57a4b50
Jared Neutel <Jared@Macintosh-2.local>
2017-04-28 14:25:19 -0700

    app

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,68 @@
+// ===============================================================================
+// LOAD DATA
+// We are linking our routes to a series of "data" sources.
+// These data sources hold arrays of information on table-data, waitinglist, etc.
+// ===============================================================================
+
+var friendData = require("../data/friends.js");
+
+
+
+// ===============================================================================
+// ROUTING
+// ===============================================================================
+
+module.exports = function(app) {
+    // API GET Requests
+    // Below code handles when users "visit" a page.
+    // In each of the below cases when a user visits a link
+    // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
+    // ---------------------------------------------------------------------------
+
+    app.get("/api/friends", function(req, res) {
+        res.json(friendData);
+    });
+
+
+    // API POST Requests
+    // Below code handles when a user submits a form and thus submits data to the server.
+    // In each of the below cases, when a user submits form data (a JSON object)
+    // ...the JSON is pushed to the appropriate JavaScript array
+    // (ex. User fills out a reservation request... this data is then sent to the server...
+    // Then the server saves the data to the tableData array)
+    // ---------------------------------------------------------------------------
+
+    app.post("/api/friends", function(req, res) {
+
+        var potentialFriend = req.body;
+        var overallDiff = [];
+        var potentialMatches = [];
+        // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
+        // It will do this by sending out the value "true" have a table
+        if (friendData.length >= 1) {
+            friendData.forEach(function(user) {
+                var difference = 0;
+
+                var databaseTotal = user.total;
+                var thisTotal = potentialFriend.total;
+                var differenceTemp = parseInt(databaseTotal) - parseInt(thisTotal);
+                difference += Math.abs(differenceTemp);
+
+                overallDiff.push(difference);
+
+            });
+
+            var minDiff = Math.min.apply(Math, overallDiff);
+            for (var i = 0; i < overallDiff.length; i++) {
+                if (overallDiff[i] === minDiff) {
+                    potentialMatches.push(friendData[i]);
+                }
+            }
+            res.json(potentialMatches);
+
+        }
+
+        friendData.push(potentialFriend);
+
+    });
+};
--- server.js
+++ server.js
@@ -1,63 +1,43 @@
-// Dependencies
-// =============================================================
+// ==============================================================================
+// DEPENDENCIES
+// Series of npm packages that we will use to give our server useful functionality
+// ==============================================================================
+
 var express = require("express");
 var bodyParser = require("body-parser");
-var path = require("path");
 
-// Sets up the Express App
-// =============================================================
+// ==============================================================================
+// EXPRESS CONFIGURATION
+// This sets up the basic properties for our express server
+// ==============================================================================
+
+// Tells node that we are creating an "express" server
 var app = express();
-var PORT = 3000;
 
-// Sets up the Express app to handle data parsing
+// Sets an initial port. We"ll use this later in our listener
+var PORT = process.env.PORT || 3000;
+
+// BodyParser makes it possible for our server to interpret data sent to it.
+// The code below is pretty standard.
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
-var applicants = [];
+// ================================================================================
+// ROUTER
+// The below points our server to a series of "route" files.
+// These routes give our server a "map" of how to respond when users visit or request data from various URLs.
+// ================================================================================
 
-//Routes
-// =============================================================
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
 
-// Basic route that sends the user first to the AJAX Page
-app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "home.html"));
-});
+// ==============================================================================
+// LISTENER
+// The below code effectively "starts" our server
+// ==============================================================================
 
-app.get("/survey", function(req, res) {
-    res.sendFile(path.join(__dirname, "survey.html"));
-});
-
-app.post("/api/new", function(req, res) {
-    var newSurvey = req.body;
-
-    console.log(newSurvey);
-
-    applicants.push(newSurvey);
-
-    res.json(newSurvey);
-
-});
-
-app.get("/api/:applicants?", function(req, res) {
-    var chosen = req.params.applicants;
-
-    if (chosen) {
-        console.log(chosen);
-
-        for (var i = 0; i < applicants.length; i++) {
-            if (chosen === applicants[i].routeName) {
-                return res.json(applicants[i]);
-            }
-        }
-        return res.json(false);
-    }
-    return res.json(applicants);
-});
-
-// Starts the server to begin listening
-// =============================================================
 app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
+  console.log("App listening on PORT: " + PORT);
 });
