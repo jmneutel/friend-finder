7bdc807f3a7a5688f1526259cc5d43bae0400043
Jared Neutel <Jared@Macintosh-2.local>
2017-04-28 15:06:04 -0700

    css

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -37,8 +37,7 @@ module.exports = function(app) {
         var potentialFriend = req.body;
         var overallDiff = [];
         var potentialMatches = [];
-        // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
-        // It will do this by sending out the value "true" have a table
+   
         if (friendData.length >= 1) {
             
             friendData.forEach(function(friend) {
